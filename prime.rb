7eca5c263c84a9f88e1987f65548de39a749d1e9
Jacqueline Lee <anginblu@gmail.com>
2017-11-16 13:16:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 def prime?(number)
   prime = ["2", "3", "5", "7", "11", "13", "17", "19", "23", "29", "31", "37", "41", "43", "47", "53", "59", "61", "67", "71", "73", "79", "83", "89", "97"]
-
-  prime.each do |i|
-    if i.include?(number.to_s)
+    if prime.include?(number)
       true
     elsif number%i == 0 || number < 2
       false
