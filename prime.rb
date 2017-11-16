c4e6844c66205cfc8a88ee932a7eeca5d8e86a9c
Jacqueline Lee <anginblu@gmail.com>
2017-11-16 13:20:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,11 @@
 def prime?(number)
   prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
-  prime.each do |i|
-    if i == number
+  if prime.any? {|i| i == number}
       true
-    elsif number%i == 0 || number < 2
+  elsif number%i == 0 || number < 2
       false
-    else
+  else
       true
     end
   end
