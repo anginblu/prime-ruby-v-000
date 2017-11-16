fb97649a8c07556cdc2e1d3cd18d009f3c943ec2
Jacqueline Lee <anginblu@gmail.com>
2017-11-16 12:57:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,14 @@
 def prime?(number)
-  if number%2 != 0 && number > 1
-    true
-  elsif number == 2
-    true
-  else
+  i = 3
+  if number%2 == 0 || number <= 1
     false
+  else
+    while i * i <= number
+      if number%i == 0
+        false
+      else
+        i += 2
+    end
+  end
   end
 end# Add  code here!
