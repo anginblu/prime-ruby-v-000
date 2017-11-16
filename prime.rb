ffa6f8ef305cbc44aa417d7b5ad8206bbf5c97f3
Jacqueline Lee <anginblu@gmail.com>
2017-11-16 13:08:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,7 @@
 def prime?(number)
   i = 3
-  if number <= 1 || number%2 == 0 || number%3 == 0 || number%5 == 0 || number%7 == 0 || number%11 == 0 || number%13 == 0 || number%17 == 0 || number%19 == 0 || 
+  if number <= 1 || number%2 == 0 || number%3 == 0 || number%5 == 0 || number%7 == 0 || number%11 == 0 || number%13 == 0 || number%17 == 0 || number%19 == 0 || number%23 == 0 || number%41 == 0 || number%43 == 0 || number%53 == 0
     false
-  elsif
-    while i * i <= number
-      if number%i == 0
-        false
-      else
-        true
-      end
-      i += 2
-    end
-  end
+  else
+    true
 end# Add  code here!
